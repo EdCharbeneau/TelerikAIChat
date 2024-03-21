e00bcfcafa26d565aff44b829422e45644b17bf4
Ed Charbeneau <ed.charbeneau@gmail.com>
2024-03-21 12:21:01 -0400

    Uses Azure OpenAI APIs instead of open source APIs.

--- TelerikBlazorApp1/OpenAiService.cs
+++ TelerikBlazorApp1/OpenAiService.cs
@@ -1,5 +1,7 @@
-﻿using OpenAI_API.Chat;
-using OpenAI_API.Models;
+﻿using Azure;
+using Azure.AI.OpenAI;
+//using OpenAI_API.Chat;
+//using OpenAI_API.Models;
 using TelerikBlazorApp1.Client;
 
 namespace TelerikBlazorApp1
@@ -8,17 +10,29 @@ namespace TelerikBlazorApp1
     {
         public async Task<string> MakeAiRequest(string prompt)
         {
-            var api = new OpenAI_API.OpenAIAPI(config["OpenAiKey"]);
-            var result = await api.Chat.CreateChatCompletionAsync(new ChatRequest()
+            OpenAIClient client = new OpenAIClient(config["OpenAiKey"]
+                , new OpenAIClientOptions());
+            //    var api = new OpenAI_API.OpenAIAPI(config["OpenAiKey"]);
+            //    var result = await api.Chat.CreateChatCompletionAsync(new ChatRequest()
+            //    {
+            //        Model = Model.ChatGPTTurbo,
+            //        Temperature = 0.5,
+            //        MaxTokens = 500,
+            //        Messages = new ChatMessage[] {
+            //    new ChatMessage(ChatMessageRole.User, prompt)
+            //}
+            //    });
+            var chatCompletionsOptions = new ChatCompletionsOptions()
             {
-                Model = Model.ChatGPTTurbo,
-                Temperature = 0.5,
-                MaxTokens = 500,
-                Messages = new ChatMessage[] {
-            new ChatMessage(ChatMessageRole.User, prompt)
-        }
-            });
-            return result.ToString();
+                DeploymentName = "gpt-3.5-turbo", // Use DeploymentName for "model" with non-Azure clients
+                Messages =
+                {
+                    new ChatRequestUserMessage(prompt),
+                }
+            };
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
+            ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
+            return responseMessage.Content;
         }
     }
 
