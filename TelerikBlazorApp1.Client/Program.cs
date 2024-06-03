98b41b1ebd21df023747b8e9e6c76ef1cbc4d302
Ed Charbeneau <ed.charbeneau@gmail.com>
2024-06-02 20:34:58 -0400

    Application theme is selectable

--- TelerikBlazorApp1.Client/Program.cs
+++ TelerikBlazorApp1.Client/Program.cs
@@ -7,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IOpenAiService, OpenAiHttpService>();
 builder.Services.AddScoped<ConversationState>();
+builder.Services.AddScoped<ThemeState>();
 builder.Services.AddScoped<ITextToSpeechService, TextToSpeechService>();
 builder.Services.AddSpeechRecognitionServices();
 builder.Services.AddBlazoredLocalStorageAsSingleton();
